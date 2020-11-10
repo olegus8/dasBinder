6e6f7e48adbc7ff14b6310a4be27ab58247880fc
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-10 17:28:49 -0500

    working on modules

--- examples/test/generated_bindings.cpp
+++ examples/test/generated_bindings.cpp
@@ -64,17 +64,6 @@ struct StructWithArrayAnnotation
     virtual bool canCopy() const override { return true; }
     virtual bool canMove() const override { return true; }
 };
-MAKE_TYPE_FACTORY(StructWithChars, StructWithChars);
-struct StructWithCharsAnnotation
-: public ManagedStructureAnnotation<StructWithChars,true,true> {
-    StructWithCharsAnnotation(ModuleLibrary & ml)
-    : ManagedStructureAnnotation ("StructWithChars", ml) {
-        addField<DAS_BIND_MANAGED_FIELD(some_chars)>("some_chars");
-    }
-    virtual bool isLocal() const override { return true; }
-    virtual bool canCopy() const override { return true; }
-    virtual bool canMove() const override { return true; }
-};
 
 class Module_generatedBindings : public Module {
 public:
@@ -97,7 +86,6 @@ public:
         addAnnotation(make_smart<FirstStructAnnotation>(lib));
         addAnnotation(make_smart<SecondStructAnnotation>(lib));
         addAnnotation(make_smart<StructWithArrayAnnotation>(lib));
-        addAnnotation(make_smart<StructWithCharsAnnotation>(lib));
     }
 };
 
