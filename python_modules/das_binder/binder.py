c0ca988d0a7b37b8407327edeb1e1066340baab8
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-11 07:41:45 -0500

    working on modules

--- python_modules/das_binder/binder.py
+++ python_modules/das_binder/binder.py
@@ -179,6 +179,8 @@ class C_TranslationUnit(LoggingObject):
         out, err, exit_code = run_exec(cmd)
         self.__root = json.loads(out)
         self.__config = config
+        self.__cached_structs = None
+        self.__cached_enums = None
 
     def __get_nodes(self, node_class, configure_fn):
         for inner in self.__root['inner']:
@@ -192,13 +194,17 @@ class C_TranslationUnit(LoggingObject):
 
     @property
     def enums(self):
-        return self.__get_nodes(node_class=C_Enum,
-            configure_fn=self.__config.configure_enum)
+        if self.__cached_enums is None:
+            self.__cached_enums = list(self.__get_nodes(node_class=C_Enum,
+                configure_fn=self.__config.configure_enum))
+        return self.__cached_enums
 
     @property
     def structs(self):
-        return self.__get_nodes(node_class=C_Struct,
-            configure_fn=self.__config.configure_struct)
+        if self.__cached_structs is None:
+            self.__cached_structs = list(self.__get_nodes(node_class=C_Struct,
+                configure_fn=self.__config.configure_struct))
+        return self.__cached_structs
 
 
 class C_InnerNode(object):
