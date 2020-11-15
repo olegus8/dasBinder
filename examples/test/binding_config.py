dc391735d773e338d9254dc152c2224c1be7db80
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-15 12:36:58 -0500

    working on modules

--- examples/test/binding_config.py
+++ examples/test/binding_config.py
@@ -6,3 +6,7 @@ class Config(ConfigBase):
     @property
     def das_module_name(self):
         return 'generatedBindings'
+
+    @property
+    def c_header_to_bind(self):
+        return 'header_to_bind.h'
