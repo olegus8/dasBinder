b0ab08cabad4c85cd444da3ffc3fc41f28dad53a
Oleg Plakhotniuk <olegus8@gmail.com>
2021-02-08 16:12:03 -0500

    Splitting binding .cpp into parts.

--- python_modules/das_binder/main.py
+++ python_modules/das_binder/main.py
@@ -1,4 +1,5 @@
 import sys
+import doctest
 from os import path
 
 if __name__ == '__main__':
@@ -7,5 +8,7 @@ if __name__ == '__main__':
         path.join(path.dirname(__file__), '..',
             '..', 'dasShared', 'python_modules'),
     ]
+    import binder
     from binder import Binder
-    Binder(argv=sys.argv).run()
+    doctest.testmod(binder)
+    binder.Binder(argv=sys.argv).run()
